f6c364c64e963b0146ecb0ed9109050674634f01
roridev <roridev@protonmail.com>
2023-05-16 20:53:17 -0300

    [migrator] Add create `Server` table migration

--- src/migrator/mod.rs
+++ src/migrator/mod.rs
@@ -18,6 +18,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20230503_000003_create_session_table::Migration),
             Box::new(m20230503_000004_create_user_table::Migration),
             Box::new(m20230503_000005_create_discord_user_table::Migration),
+            Box::new(m20230503_000006_create_server_table::Migration),
         ]
     }
 }
