541241612b0ca780019ee97e81a09bb3b78e11e3
roridev <roridev@protonmail.com>
2023-05-16 17:37:51 -0300

    [migrator] Add create `DiscordUser` table migration

--- src/migrator/mod.rs
+++ src/migrator/mod.rs
@@ -2,6 +2,7 @@ mod m20230503_000001_create_nonce_table;
 mod m20230503_000002_create_account_table;
 mod m20230503_000003_create_session_table;
 mod m20230503_000004_create_user_table;
+mod m20230503_000005_create_discord_user_table;
 
 use sea_orm_migration::prelude::*;
 
@@ -15,6 +16,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20230503_000002_create_account_table::Migration),
             Box::new(m20230503_000003_create_session_table::Migration),
             Box::new(m20230503_000004_create_user_table::Migration),
+            Box::new(m20230503_000005_create_discord_user_table::Migration),
         ]
     }
 }
