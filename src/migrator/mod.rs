0296313bbde8fae1e17ffe850f144782a6079e88
Vi <roridev@iMac-de-Alice.local>
2023-05-24 11:58:17 -0300

    [sql] Add `Modpack` table

--- src/migrator/mod.rs
+++ src/migrator/mod.rs
@@ -5,6 +5,7 @@ mod m20230503_000004_create_user_table;
 mod m20230503_000005_create_discord_user_table;
 mod m20230503_000006_create_server_table;
 mod m20230503_000007_create_connection_table;
+mod m20230503_000008_create_modpack_table;
 
 use sea_orm_migration::prelude::*;
 
