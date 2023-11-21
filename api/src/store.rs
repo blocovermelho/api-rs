47b480ca680122516b09082708187f6ff1846602
Alice Isabel <roridev@protonmail.com>
2023-11-21 14:44:44 -0300

    Add `drop_user` function

--- api/src/store.rs
+++ api/src/store.rs
@@ -86,6 +86,10 @@ impl Store {
         }
     }
 
+    pub fn drop_user(&mut self, uuid: &Uuid) -> Option<User> {
+        self.users.remove(uuid)
+    }
+
     pub fn add_server(&mut self, server: Server) -> bool {
         match self.get_server(&server.uuid) {
             Some(_) => false,
