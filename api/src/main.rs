bd3c2648c9fa3f25508c43e87ec64e9a4902a778
Alice Isabel <roridev@protonmail.com>
2023-11-30 19:04:04 -0300

    Change server binding to 0.0.0.0 instead of localhost

--- api/src/main.rs
+++ api/src/main.rs
@@ -169,7 +169,7 @@ async fn main() {
         .layer(stack)
         .layer(TraceLayer::new_for_http());
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
