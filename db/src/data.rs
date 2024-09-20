c927ad3ee0c371ca5e2df49befcb9ca8f7c81c74
alikindsys <alice@blocovermelho.org>
2024-09-19 22:18:57 -0300

    Add `PartialEq` derive to `Viewport`

--- db/src/data.rs
+++ db/src/data.rs
@@ -127,7 +127,7 @@ pub enum BanActor {
     Staff(Uuid),
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct Loc {
     pub dim: String,
     pub x: f64,
@@ -146,7 +146,7 @@ impl Default for Loc {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct Viewport {
     pub loc: Loc,
     pub yaw: f64,
