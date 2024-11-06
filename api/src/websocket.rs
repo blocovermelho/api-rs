fe5531e4159a9e0b713f0fb833d3655cf82e617b
alikindsys <alice@blocovermelho.org>
2024-11-06 00:24:08 -0300

    Remove `CidrAwk` handling
    This will be superceded by the new discord message based 2FA flow.

--- api/src/websocket.rs
+++ api/src/websocket.rs
@@ -28,10 +28,7 @@ async fn link_socket(socket: WebSocket<MessageOut, MessageIn>, state: AppState) 
                     MessageIn::LinkRequest(request) => {
                         handle_link_request(request, &read_state).await;
                     }
-
-                    MessageIn::CidrAwk(nonce) => {
-                        handle_cidr_awk(nonce, &read_state).await;
-                    }
+                    _ => { handle_unknown(&read_state).await; }
                 },
 
                 Ok(_) => {
@@ -69,49 +66,6 @@ async fn handle_error(e: axum_typed_websockets::Error<serde_json::Error>, state:
         error: e.to_string(),
     })
     .await;
-}
-
-async fn handle_cidr_awk(nonce: String, state: &AppState) {
-    let user_id = timeout(Duration::from_secs(5), state.chs.verify.recv(nonce.clone())).await;
-    let mut buff = state.chs.messages.0.lock().await;
-
-    if let Err(_) = user_id {
-        let _ = buff.send(MessageOut::Error { source_event: "cidr_awk".to_owned(), error: "Timed Out".to_owned() }).await;
-        return;
-    }
-
-    if let Ok(Ok(user_id)) = user_id {
-        let store = state.data.lock().await;
-        let user = store.get_user_from_discord(user_id);
-        
-        if let Some(user) = user {
-            let _ = buff.send(MessageOut::CidrSynAwk(user.clone())).await;
-        } else {
-            let _ = buff.send(MessageOut::Error { source_event: "cidr_awk".to_owned(), error: "User not found.".to_owned() }).await;
-        }        
-        return;
-    }
-
-    if let Ok(Err(_)) = user_id {
-        // Tried reusing an expired channel. This can happen when users retry the command with the same token.
-        // In this case we can't just rely on the channel to provide us the discord id, which requires an reach-around
-        // to the store to get if that exists or not.
-
-        let store = state.data.lock().await;
-        let holder = store.get_handshake_holder(&nonce);
-
-        if let Some(holder) = holder {
-            let _ = buff.send(MessageOut::CidrSynAwk(holder)).await;
-            return;
-        } else {
-            let _ = buff.send(MessageOut::Error { source_event: "cidr_awk".to_owned(), error: "Invalid Token.".to_owned() }).await;
-            return;
-        }
-    }
-
-
-    let _ = buff.send(MessageOut::Error { source_event: "cidr_awk".to_owned(), error: "Unreachable.".to_owned() }).await;
-    return;
 }
 
 async fn handle_link_request(request: Uuid, state: &AppState) {
