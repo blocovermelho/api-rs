13caa40f137fdd89bccc2e768423807f98b93de3
alikindsys <alice@blocovermelho.org>
2025-01-04 21:32:56 -0300

    Add autocompletion for server names

--- discord/src/autocomplete.rs
+++ discord/src/autocomplete.rs
@@ -37,3 +37,20 @@ pub async fn players<'a>(ctx: Context<'_>, partial: &'a str) -> impl Stream<Item
     stream::iter(users)
 }
 
+/// Autocompletion for server names
+pub async fn servers<'a>(ctx: Context<'_>, partial: &'a str) -> impl Stream<Item = String> + 'a {
+    let db = &ctx.data().db;
+    let server_ids = db.get_all_servers().await.unwrap_or_default();
+
+    let mut servers: Vec<_> = vec![];
+
+    for id in server_ids {
+        if let Ok(server) = db.get_server(&id).await {
+            if server.name.starts_with(partial) || partial.is_empty() {
+                servers.push(server.name)
+            }
+        }
+    }
+
+    stream::iter(servers)
+}
