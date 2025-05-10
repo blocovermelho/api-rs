1528d3cbc233b76d2c43bc52b7019754931fc95c
alikindsys <alice@blocovermelho.org>
2025-05-09 21:30:23 -0300

    Add a prelude for making tests

--- api/src/tests.rs
+++ api/src/tests.rs
@@ -1,6 +1,7 @@
 pub mod auth;
 pub mod discord;
 pub mod migration;
+pub mod prelude;
 pub mod server;
 pub mod user;
 
---
+++ api/src/tests/prelude.rs
@@ -0,0 +1,48 @@
+pub use crate::routes::prelude::*;
+
+pub fn get_state() -> State<Arc<AppState>> {
+    State(Arc::new(AppState::mock()))
+}
+
+pub mod stub {
+    use std::net::Ipv4Addr;
+
+    use db::data::stub::{AccountStub, UserStub};
+    use uuid::Uuid;
+
+    use crate::tests::prelude::query_params::LoginAttempt;
+
+    pub fn login_attempt(name: &str, password: &str) -> LoginAttempt {
+        LoginAttempt {
+            uuid: uuid_mc::PlayerUuid::new_with_offline_username(name)
+                .as_uuid()
+                .clone(),
+            ip: Ipv4Addr::LOCALHOST.into(),
+            password: password.to_string(),
+        }
+    }
+
+    pub fn user(name: &str, discord_id: &str) -> UserStub {
+        UserStub {
+            uuid: uuid_mc::PlayerUuid::new_with_offline_username(name)
+                .as_uuid()
+                .clone(),
+            username: name.to_string(),
+            discord_id: discord_id.to_string(),
+        }
+    }
+
+    /// This function *hashes* the password
+    /// To be used with database functions that expect hashed input.
+    pub fn account_hashed(uuid: Uuid, password: &str) -> AccountStub {
+        AccountStub {
+            uuid,
+            password: bcrypt::hash(password, 12).unwrap().into(),
+        }
+    }
+    /// This function *does not* hash the password.
+    /// To be used with API handlers.
+    pub fn account(uuid: Uuid, password: &str) -> AccountStub {
+        AccountStub { uuid, password: password.into() }
+    }
+}
