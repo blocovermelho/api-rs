4f3cebfe34221fd2e420104fed88729f744cac15
alikindsys <alice@blocovermelho.org>
2025-10-11 18:20:39 -0300

    Add intial transformation / ingress module

---
+++ api/src/core/trans/db.rs
@@ -0,0 +1,169 @@
+use std::sync::Arc;
+
+// Transformations and ingress for databse datatypes
+use chrono::TimeDelta;
+use uuid::Uuid;
+
+use crate::{
+    core::types::{enums::ConnectionType, structs::*},
+    db::{
+        data::{self as dbd},
+        interface::{DataSource, NetworkProvider},
+    },
+};
+
+const SESSION_LEASE: TimeDelta = TimeDelta::minutes(10);
+
+impl From<dbd::Allowlist> for Session {
+    fn from(val: dbd::Allowlist) -> Self {
+        Self {
+            profile: val.uuid,
+            last_seen: val.last_join,
+            expires_at: val.last_join + SESSION_LEASE,
+            network: val.get_network(),
+        }
+    }
+}
+#[derive(Debug)]
+pub enum ProfileConversionError {
+    NoAccountError,
+    UnlinkedUserError,
+    UnknownUUIDError,
+}
+
+#[async_trait::async_trait]
+pub trait TryIngest<T, D: DataSource>: Sized {
+    type Error;
+    async fn try_ingest(value: T, source: Arc<D>) -> Result<Self, Self::Error>
+    where
+        D: 'async_trait;
+}
+
+#[async_trait::async_trait]
+impl<D: DataSource> TryIngest<dbd::User, D> for Profile {
+    type Error = ProfileConversionError;
+
+    async fn try_ingest(value: dbd::User, source: Arc<D>) -> Result<Self, Self::Error>
+    where
+        D: 'async_trait,
+    {
+        let account = source
+            .get_account(&value.uuid)
+            .await
+            .map_err(|_| ProfileConversionError::NoAccountError)?;
+
+        Ok(Self {
+            id: value.uuid,
+            username: value.username,
+            discord_id: value.discord_id,
+            hash_password: account.password,
+            connections: vec![],
+            created_at: get_timestamp_from_uuid(&value.uuid, value.created_at),
+            last_seen: account.current_join,
+        })
+    }
+}
+
+#[allow(clippy::as_conversions)]
+fn get_timestamp_from_uuid(
+    uuid: &Uuid, fallback: chrono::DateTime<chrono::Utc>,
+) -> chrono::DateTime<chrono::Utc> {
+    if let Some(timestamp) = uuid.get_timestamp() {
+        let (secs, nsecs) = timestamp.to_unix();
+        chrono::DateTime::from_timestamp(secs as i64, nsecs).unwrap()
+    } else {
+        fallback
+    }
+}
+
+#[async_trait::async_trait]
+impl<D: DataSource> TryIngest<dbd::Account, D> for Profile {
+    type Error = ProfileConversionError;
+
+    async fn try_ingest(value: dbd::Account, source: Arc<D>) -> Result<Self, Self::Error>
+    where
+        D: 'async_trait,
+    {
+        let user = source
+            .get_user_by_uuid(&value.uuid)
+            .await
+            .map_err(|_| ProfileConversionError::UnlinkedUserError)?;
+
+        Ok(Self {
+            id: value.uuid,
+            username: user.username,
+            discord_id: user.discord_id,
+            hash_password: value.password,
+            connections: vec![],
+            created_at: get_timestamp_from_uuid(&value.uuid, user.created_at),
+            last_seen: value.current_join,
+        })
+    }
+}
+#[derive(Debug)]
+pub enum ConnectionConversionError {
+    UnknownTypeError,
+    UnknownProfile,
+    UnknownIssuer,
+    InvalidMojangUUIDError,
+}
+
+#[async_trait::async_trait]
+impl<D: DataSource> TryIngest<dbd::Connection, D> for Connection {
+    type Error = ConnectionConversionError;
+
+    async fn try_ingest(value: dbd::Connection, source: Arc<D>) -> Result<Self, Self::Error>
+    where
+        D: 'async_trait,
+    {
+        let extra = ConnectionType::try_from((value.kind, value.data))?;
+        let user = source
+            .get_user_by_uuid(&value.profile)
+            .await
+            .map_err(|_| ConnectionConversionError::UnknownProfile)?;
+
+        let profile = Profile::try_ingest(user, source)
+            .await
+            .map_err(|_| ConnectionConversionError::UnknownProfile)?;
+
+        Ok(Self { issuer: value.issuer, profile, extra })
+    }
+}
+
+pub enum ConnectionTypeConversionError {
+    UnknownConnectionType,
+}
+
+impl TryFrom<(String, String)> for ConnectionType {
+    type Error = ConnectionConversionError;
+
+    fn try_from(value: (String, String)) -> Result<Self, Self::Error> {
+        match value.0.as_str() {
+            "bv:bedrock_link" => Ok(Self::BedrockUsername(value.1.trim().to_owned())),
+            "bv:mojang_uuid" => {
+                if let Ok(uuid) = value.1.parse() {
+                    Ok(Self::MojangUuid(uuid))
+                } else {
+                    Err(ConnectionConversionError::InvalidMojangUUIDError)
+                }
+            }
+            _ => Err(ConnectionConversionError::UnknownTypeError),
+        }
+    }
+}
+
+impl From<Connection> for dbd::Connection {
+    fn from(value: Connection) -> Self {
+        let (kind, data) = match value.extra {
+            ConnectionType::BedrockUsername(username) => ("bv:bedrock_link", username),
+            ConnectionType::MojangUuid(uuid) => ("bv:mojang_uuid", uuid.into()),
+        };
+
+        Self {
+            profile: value.profile.id,
+            issuer: value.issuer,
+            kind: kind.to_string(),
+            data,
+        }
+    }
+}
