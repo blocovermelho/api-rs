75f7bc4e6f6f8a79031e468895a6ceda2e340815
alikindsys <alice@blocovermelho.org>
2025-11-09 17:08:28 -0300

    [discord] Add modal for creating servers

--- api/src/discord/render/modal.rs
+++ api/src/discord/render/modal.rs
@@ -38,3 +38,20 @@ pub struct NewPassword {
     #[max_length = 64]
     pub password: String,
 }
+
+#[derive(Debug, poise::Modal)]
+#[name = "Bloco Vermelho - Criação de Servidor"]
+pub struct NewServer {
+    #[name = "Nome do servidor"]
+    #[placeholder = "Digite o nome aqui. Não pode repetir um nome já existente."]
+    #[min_length = 8]
+    pub name: String,
+    #[name = "Jogo do servidor"]
+    #[placeholder = "Factorio"]
+    pub game: String,
+    #[name = "Versões suportadas"]
+    #[placeholder = "Uma versão OU lista de versões separadas por vírgula. Ex: 1.12.2,1.12.3"]
+    pub versions: String,
+    #[name = "Número máximo de jogadores"]
+    pub max_players: String,
+}
